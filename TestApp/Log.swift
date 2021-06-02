1c0b2f35764d8e985743813fc4f35e5f096b21c9
haticesebli <hatice.sebli@gmail.com>
2021-06-01 23:46:09 -0400

    Log file added

---
+++ TestApp/Log.swift
@@ -0,0 +1,14 @@
+//
+//  Log.swift
+//  TestApp
+//
+//  Created by Hatice Sebli on 6/1/21.
+//
+
+import Foundation
+
+class Log {
+    public func print(){
+        print("hatice log event")
+    }
+}
